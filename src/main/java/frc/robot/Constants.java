5d6dee6e8f35c09d47e5610b252661ab1d74699c
lyr0621 <75480109+lyr0621@users.noreply.github.com>
2024-07-06 04:45:07 -0400

    added calibration code and calibration file

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,7 +4,8 @@
 
 package frc.robot;
 
-import frc.robot.utils.MechanismControlHelpers.MapleSimplePIDController;
+import frc.robot.tests.WheelsCalibrationCTRE;
+import frc.robot.utils.MechanismControl.MapleSimplePIDController;
 
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
@@ -48,14 +49,14 @@ public final class Constants {
         }
         public static final ChassisType chassisType = ChassisType.CTRE_ON_CANIVORE;
 
-        public static final String DEFAULT_CHASSIS_CANIVORE = "ChassisCanivore";
+        public static final String CHASSIS_CANBUS = "ChassisCanivore";
 
         public static final int ODOMETRY_CACHE_CAPACITY = 10;
         public static final double ODOMETRY_FREQUENCY = 250;
         public static final double ODOMETRY_WAIT_TIMEOUT_SECONDS = 0.02;
     }
 
-    public static final class CalibrationConfigs {
+    public static final class ChassisDefaultConfigs {
         public static final int DEFAULT_GYRO_PORT = 0;
         public static final double DEFAULT_GEAR_RATIO = 6.12;
         public static final double DEFAULT_WHEEL_RADIUS_METERS = 0.051; // 2 inch
@@ -68,14 +69,42 @@ public final class Constants {
         public static final double DEFAULT_MAX_ANGULAR_VELOCITY_DEGREES_PER_SECOND = 540;
     }
 
+    public static final class WheelCalibrationConfigs {
+        public static final class WheelToBeCalibrated {
+            public final String name;
+            public final int drivingMotorID, steeringMotorID, encoderID, drivingMotorPortOnPDP, steeringMotorPortOnPDP;
+            public boolean steeringMotorInverted;
+
+            public WheelToBeCalibrated(String name, int drivingMotorID, int steeringMotorID, int encoderID) {
+                this(name, drivingMotorID, steeringMotorID, encoderID, -1, -1);
+            }
+
+            private WheelToBeCalibrated(String name, int drivingMotorID, int steeringMotorID, int encoderID, int drivingMotorPortOnPDP, int steeringMotorPortOnPDP) {
+                this.name = name;
+                this.drivingMotorID = drivingMotorID;
+                this.steeringMotorID = steeringMotorID;
+                this.encoderID = encoderID;
+                this.drivingMotorPortOnPDP = drivingMotorPortOnPDP;
+                this.steeringMotorPortOnPDP = steeringMotorPortOnPDP;
+                this.steeringMotorInverted = false;
+            }
+        }
+        public static final WheelToBeCalibrated[] wheelsToBeCalibrated = new WheelToBeCalibrated[] {
+                new WheelToBeCalibrated("FrontLeft", 3, 4, 10),
+                new WheelToBeCalibrated("FrontRight", 6, 5, 11),
+                new WheelToBeCalibrated("BackLeft", 1, 2, 9),
+                new WheelToBeCalibrated("BackRight", 8, 7, 12)
+        };
+    }
+
     public static final class SwerveModuleConfigs {
-        public static final double MINIMUM_USAGE_SPEED_METERS_PER_SECOND = CalibrationConfigs.DEFAULT_MAX_VELOCITY_METERS_PER_SECOND * 0.03;
+        public static final double MINIMUM_USAGE_SPEED_METERS_PER_SECOND = ChassisDefaultConfigs.DEFAULT_MAX_VELOCITY_METERS_PER_SECOND * 0.03;
         public static final double NON_USAGE_TIME_RESET_SWERVE = 0.5;
 
         public static final MapleSimplePIDController.SimplePIDProfile steerHeadingCloseLoopConfig = new MapleSimplePIDController.SimplePIDProfile(
                 0.9,
                 Math.toRadians(65),
-                0.01,
+                0.02,
                 Math.toRadians(1.5),
                 0.05,
                 true
