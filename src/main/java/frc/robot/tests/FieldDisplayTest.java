4d6c8511a4691fbc8166faef5603a9870b863b96
caTrixLiu <lyr20070621@gmail.com>
2024-08-10 07:02:57 -0400

    added advantage scope assets and tested it

--- src/main/java/frc/robot/tests/FieldDisplayTest.java
+++ src/main/java/frc/robot/tests/FieldDisplayTest.java
@@ -1,35 +1,17 @@
 package frc.robot.tests;
 
-import com.pathplanner.lib.path.PathPlannerPath;
-import com.pathplanner.lib.path.PathPoint;
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.utils.CompetitionFieldUtils.FieldObjects.Crescendo2024FieldObjects;
-import frc.robot.utils.CompetitionFieldUtils.FieldObjects.RobotOnFieldDisplay;
-import frc.robot.utils.CompetitionFieldUtils.MapleCompetitionField;
+import frc.robot.utils.MapleTimeUtils;
 import org.littletonrobotics.junction.Logger;
 
 public class FieldDisplayTest extends Command {
-    private MapleCompetitionField field;
-
-    @Override
-    public void initialize() {
-        final RobotOnFieldDisplay mainRobot = () -> new Pose2d(4, 4, new Rotation2d()),
-                anotherRobot = () -> new Pose2d(4, 5, new Rotation2d());
-        field = new MapleCompetitionField(mainRobot);
-        field.addObject(anotherRobot);
-
-        field.addObject(new Crescendo2024FieldObjects.NoteOnFieldStatic(new Translation2d( 2, 1)));
-
-        PathPlannerPath path = PathPlannerPath.fromPathFile("opponent cycle path 2");
-        Logger.recordOutput("Test Path", path.getPathPoses().toArray(Pose2d[]::new));
-        Logger.recordOutput("Test Path distance", path.getAllPathPoints().stream().mapToDouble((pathPoint -> pathPoint.distanceAlongPath)).toArray());
-    }
-
     @Override
     public void execute() {
-        field.updateObjectsToDashboardAndTelemetry();
+        Logger.recordOutput("Test/Robot", new Pose2d(3, 3, new Rotation2d()));
+        Logger.recordOutput("Test/Mechanism", new Pose3d(-0.25, 0.245, 0.06, new Rotation3d(0, -Math.toRadians(15 + 8 + 15 * Math.sin(MapleTimeUtils.getLogTimeSeconds())), 0)));
     }
 }
