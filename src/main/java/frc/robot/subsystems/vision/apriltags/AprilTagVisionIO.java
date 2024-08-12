9ee51574bd55a2ffb87b9e2c9c5e44fc4ea3ee8a
caTrixLiu <lyr20070621@gmail.com>
2024-08-11 22:45:36 -0400

    fixed a bug in apriltag vision

--- src/main/java/frc/robot/subsystems/vision/apriltags/AprilTagVisionIO.java
+++ src/main/java/frc/robot/subsystems/vision/apriltags/AprilTagVisionIO.java
@@ -35,7 +35,7 @@ public interface AprilTagVisionIO {
         public void fromPhotonPipeLine(PhotonPipelineResult pipelineResult, boolean cameraConnected) {
             this.cameraConnected = cameraConnected;
             this.resultsDelaySeconds = pipelineResult.getLatencyMillis() / 1000.0;
-            this.currentTargetsCount = pipelineResult.getTargets().size();
+            this.currentTargetsCount = Math.min(pipelineResult.getTargets().size(), MAX_TARGET_PER_CAMERA);
             Arrays.fill(fiducialMarksID, -1);
             Arrays.fill(bestCameraToTargets, new Transform3d());
             for (int i = 0; i < currentTargetsCount && i < MAX_TARGET_PER_CAMERA; i++) {
@@ -94,7 +94,7 @@ public interface AprilTagVisionIO {
             final int loggedCamerasAmount = table.get("camerasAmount", 0);
             if (table.get("camerasAmount", 0) != camerasAmount)
                 throw new IllegalStateException("cameras amount from log (" + loggedCamerasAmount +") does not match the settings in replay"
-                + "\n check if the code have changed");
+                        + "\n check if the code have changed");
 
             inputsFetchedRealTimeStampSeconds = table.get("inputsFetchedTimeStamp", 0.0);
             for (int i = 0; i < camerasAmount; i++)
