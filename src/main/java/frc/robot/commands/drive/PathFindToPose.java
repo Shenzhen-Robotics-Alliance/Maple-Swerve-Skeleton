fb1bd1f3b3370acc9b697a76aae404b991bea3d1
caTrixLiu <lyr20070621@gmail.com>
2024-11-12 18:21:20 +0800

    fixed a bug in auto alignment

--- src/main/java/frc/robot/commands/drive/PathFindToPose.java
+++ src/main/java/frc/robot/commands/drive/PathFindToPose.java
@@ -2,8 +2,11 @@ package frc.robot.commands.drive;
 
 import com.pathplanner.lib.auto.AutoBuilder;
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.commands.CommandOnFly;
 import frc.robot.subsystems.drive.HolonomicDriveSubsystem;
+import frc.robot.subsystems.drive.SwerveDrive;
+import frc.robot.utils.ChassisHeadingController;
 import java.util.function.Supplier;
 
 public class PathFindToPose extends CommandOnFly {
@@ -13,6 +16,8 @@ public class PathFindToPose extends CommandOnFly {
             double speedMultiplier,
             double goalEndVelocity) {
         super(() -> AutoBuilder.pathfindToPose(
-                targetPose.get(), driveSubsystem.getChassisConstrains(speedMultiplier), goalEndVelocity));
+                        targetPose.get(), driveSubsystem.getChassisConstrains(speedMultiplier), goalEndVelocity)
+                .beforeStarting(Commands.runOnce(() -> SwerveDrive.swerveHeadingController.setHeadingRequest(
+                        new ChassisHeadingController.NullRequest()))));
     }
 }
